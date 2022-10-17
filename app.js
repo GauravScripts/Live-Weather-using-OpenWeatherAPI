202b5293ee0b6b7fc6dc58b873899cf93da0c7bf
Gaurav Gupta <alwar.gaurav1@gmail.com>
2022-10-18 01:01:14 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require("express");
-const app = express();
-const https = require("https");
-app.get("/", function(req, response){
-    const url ="https://api.openweathermap.org/data/2.5/weather?q=Alwar,In&appid=158bf4aad8af166eb7df6b5285c91226&units=metric"
-    https.get(url,function(res){
-        console.log(res.statusCode,res.statusMessage);
-        res.on("data",function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp
-            const weatherD = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/"+ icon+"@2x.png"
-            response.write("<p>The weather description has been updated "+weatherD+" and</p>");
-            response.write("<h1>The temperature in alwar is " + temp+" degrees</h1>")
-            response.write("<img src="+imageURL+">")
-            response.send();            
-        })
-    })
-}
-);
+var express = require("express");
+var app = express();
+var https = require("https");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, response) {
+    response.sendFile(__dirname + "/index.html");
 
-
-
-
-app.listen(3000,function(){
-    console.log("listening on port 3000");})
+});
+app.post("/", function (req, response) {
+    
+    const query = req.body.cityName;
+    const apikey = "158bf4aad8af166eb7df6b5285c91226";
+    const unit ="metric";
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apikey+"&units="+unit;
+    https.get(url, function (res) {
+        console.log(res.statusCode, res.statusMessage);
+        res.on("data", function (data) {
+            var weatherData = JSON.parse(data);
+            var temp = weatherData.main.temp;
+            var weatherD = weatherData.weather[0].description;
+            var icon = weatherData.weather[0].icon;
+            var imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            response.write("<p>The weather description has been updated " + weatherD + " and</p>");
+            response.write("<h1>The temperature in "+query+" is " + temp + " degrees</h1>");
+            response.write("<img src=" + imageURL + ">");
+            response.send();
+        });
+    });
+});
+app.listen(3000, function () {
+    console.log("listening on port 3000");
+});
